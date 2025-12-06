81b03c8bcfa10fde349751a3d7505883b6a30b49
Guillaume Fe <guillaume.ferron@gmail.com>
2025-12-06 19:59:02 +0100

    Create ui.paths.js

---
+++ js/js/ui.paths.js
@@ -0,0 +1,6 @@
+export function renderPaths() {
+  document.getElementById("section-map").innerHTML = `
+    <h2>🗺️ Chemins</h2>
+    <p>Affichage des chemins (à implémenter dans le module complet)</p>
+  `;
+}

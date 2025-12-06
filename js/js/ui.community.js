7bc8d7a17377c57062babb4d6c7f887f41911ce7
Guillaume Fe <guillaume.ferron@gmail.com>
2025-12-06 20:00:20 +0100

    Create ui.community.js

---
+++ js/js/ui.community.js
@@ -0,0 +1,6 @@
+export function renderCommunity() {
+  document.getElementById("section-community").innerHTML = `
+    <h2>🌐 Communauté</h2>
+    <p>Recherche & publication de LifePaths (module à compléter)</p>
+  `;
+}
